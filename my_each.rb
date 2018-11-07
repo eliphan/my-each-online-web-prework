444a5e1593f31d4b7fbbe7a891f21436da8fe5bb
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-07 00:45:14 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,9 @@
-collection = [1,2,3,4]
-def my_each(collection) 
-  my_each(collection) do |i|
-  puts i
- end
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
